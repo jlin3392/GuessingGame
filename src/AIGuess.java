d6012ce5fee71cf5f7410a30b1cffefb0b40dd33
BT_1N3_34 <jlin3392@bths.edu>
2018-10-11 14:48:44 -0400

    untested

--- src/AIGuess.java
+++ src/AIGuess.java
@@ -7,11 +7,47 @@ public class AIGuess {
         System.out.println("Hello. What is your name?");
         String name = input.nextLine();
         System.out.println("Hi, " + name + "! Think of a number.");
-        System.out.println("Type the number between 1 and 100 here. I will try to guess what it is based on clues.");
-
-        String number = input.nextLine();
 
         System.out.println("Okay, now that you have your number, let me guess what it is.");
-        
+
+        int max = 100;
+        int min = 1;
+        int range = 50;
+        int guessNum = 0;
+
+        double generateGuess = Math.random();
+        int guessInt = (int)(generateGuess*max + 1);
+
+        String guess = Integer.toString(guessInt);
+        boolean correct = false;
+
+        System.out.println("Type 'higher' if the number is higher than my guess, 'lower' if the number is lower than my guess, and 'correct' if I guess correctly.");
+
+
+        while (correct == false) {
+            System.out.println("Is your number " + guess + "?");
+            String answer = input.nextLine();
+
+            if (answer.equals("higher")) {
+                generateGuess = Math.random();
+                guessInt = (int) (generateGuess * ((max - min) + 1))+ min;
+                guess = Integer.toString(guessInt);
+                guessNum++;
+                max = max;
+                min = min + range/2;
+            } else if (answer.equals("lower")) {
+                generateGuess = Math.random();
+                guessInt = (int) (generateGuess * ((max - min) + 1)) + min;
+                guess = Integer.toString(guessInt);
+                guessNum++;
+                min = min;
+                max = max - range/2;
+            } else if (answer.equals("correct")) {
+                correct = true;
+                System.out.print("Yay, I guessed correctly! ");
+                System.out.print("It took me " + guessNum + " tries to guess correctly.");
+            }
+        }
     }
 }
+
